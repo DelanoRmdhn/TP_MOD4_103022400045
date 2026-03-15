b2ffabc7f25b939e3f91569a651ec6804c714329
Delano <del.191006@gmail.com>
2026-03-16 01:13:15 +0700

    second commit

--- TP_MODUL4_103022400045/Program.cs
+++ TP_MODUL4_103022400045/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using TP_MODUL4_103022400045;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        KodePos kodePos = new KodePos();
+
+        int kode = kodePos.getKodePos("Batununggal");
+
+        System.Console.WriteLine("Kode Pos Batununggal: " + kode);
+    }
+}
